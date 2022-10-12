cab4cea5c89a5af992c73359ae977c462273c6c4
yalem123 <yalembrhankelayneh@gmail.com>
2022-10-12 00:38:09 +0000

    submit

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+--database for test
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd;
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIEVILAGES;
