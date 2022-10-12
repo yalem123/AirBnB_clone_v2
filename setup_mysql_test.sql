083b27c81246e64cd14168aebcdc2efa83c4b786
yalem123 <yalembrhankelayneh@gmail.com>
2022-10-12 00:53:49 +0000

    edit

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,6 @@
---database for test
+--create database for hbnb test
+--user hbnb with password
+--grant all privilages for hbnb test.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd;
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
