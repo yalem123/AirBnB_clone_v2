de55e567c49ecc6291e3835f90fb4c35e333c2a0
yalem123 <yalembrhankelayneh@gmail.com>
2022-11-13 20:35:12 +0300

    submit

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,7 +2,8 @@
 """Starts a Flask web application.
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
 """
 from flask import flask
 
@@ -11,8 +12,14 @@ app = flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
 
 
 if __name__ == "__main__":
